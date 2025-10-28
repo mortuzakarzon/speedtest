ccf5f1abd68ab316dd3dc264618fae800bfa54c8
Mortuza Karzon <creativeleadsmk@gmail.com>
2025-10-28 12:30:16 +0600

    new

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 app.use(cors());
 app.use(express.raw({ type: "application/octet-stream", limit: "50mb" }));
